e75da82924c5b9e84cd6cdcb822a22e7a5a0c6e9
ahmed1812 <algandel19@gmail.com>
2024-03-02 18:12:02 -0600

    create UserInfo model

---
+++ SimpleSocialMediaPlatform/SimpleSocialMediaPlatform/Models/UserInfo.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace SimpleSocialMediaPlatform.Models
+{
+    public class UserInfo
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string UserId { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string Address2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public string Image { get; set; }
+        [NotMapped]
+        public IFormFile ImageFile { get; set; }
+        public DateTime DOB { get; set; }
+        public DateTime Created { get; set; }
+    }
+}
