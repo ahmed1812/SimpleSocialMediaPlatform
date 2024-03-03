e07a24441ae7689c24f7a31835ecefaa849e7042
ahmed1812 <algandel19@gmail.com>
2024-03-02 18:09:34 -0600

    create comment model

---
+++ SimpleSocialMediaPlatform/SimpleSocialMediaPlatform/Models/Comments.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimpleSocialMediaPlatform.Models
+{
+    public class Comments
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? Body { get; set; }
+        public int UserId { get; set; }
+        //[ForeignKey("PostId")]
+        public int PostId { get; set; }
+        public DateTime CreateAt { get; set; }
+    }
+}
